c7a196a6896e68e3a54218e2e5688156168ddb19
Right0603 <raitis.skrebels03@gmail.com>
2023-09-22 17:29:09 +0300

    added a few view functions

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
+    path("article/<int:article_id>/", views.article, name="article"),
+    path("create/", views.create, name="create"),
+    path("aboutUs/", views.about_us, name="aboutUs"),
 ]
--- main/views.py
+++ main/views.py
@@ -7,4 +7,18 @@ def index(request):
     data = {
         "text": "This is start page!",
     }
-    return render(request, "startPage.html", context=data );
+
+    return render(request, "startPage.html", context=data )
+
+
+def article(request, article_id):
+    data = {
+        "text": f"This is article number {article_id}"
+    }
+    return render(request, "article.html", context=data )
+
+def create(request):
+    return render(request, "createArticle.html")
+
+def about_us(request):
+    return render(request, "aboutUs.html")
