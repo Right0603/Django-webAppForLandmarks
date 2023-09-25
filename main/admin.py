f0eb6bd7e166252e82d7ef21c7c9abcb7a5355e8
Right0603 <raitis.skrebels03@gmail.com>
2023-09-25 18:34:16 +0300

    Added 'Category' model

--- main/admin.py
+++ main/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-#Piemērs kā reģistrēt:
-
 from .models import Article
 
 admin.site.register(Article)
--- main/models.py
+++ main/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+import datetime
 
 class Article(models.Model):
     title = models.CharField(max_length=30)
     text = models.TextField()
-    image = models.ImageField()
-    pub_date = models.DateTimeField("date published")
+    image = models.ImageField(blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
      return self.title
+    
+class Category(models.Model):
+   title = models.CharField(max_length=30)
+   description = models.TextField()
+
